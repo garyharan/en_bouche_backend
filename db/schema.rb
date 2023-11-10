b07786950507b9d090671ea42919390cec5e65a6
Gary Haran <garyharan@users.noreply.github.com>
2023-11-10 17:45:41 -0500

    allow recipes to be created and tests to run

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_10_214302) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_10_220521) do
+  create_table "ingredients", force: :cascade do |t|
+    t.integer "recipe_id", null: false
+    t.string "name"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
+  end
+
   create_table "recipes", force: :cascade do |t|
     t.string "name"
     t.text "instructions"
@@ -19,4 +28,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_10_214302) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "ingredients", "recipes"
 end
