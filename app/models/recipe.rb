f6cdf3d6d625a3073962e5c55d2cc67fee6ef03e
Gary Haran <garyharan@users.noreply.github.com>
2023-11-13 11:15:15 -0500

    Easier to read ingredients in form

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,4 +4,6 @@ class Recipe < ApplicationRecord
 
   has_many :steps, -> { order(position: :asc) }, dependent: :destroy
   accepts_nested_attributes_for :steps, reject_if: :all_blank, allow_destroy: true
+
+  LANGUAGES = %w(en fr).freeze
 end
