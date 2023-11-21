5a9491a431b1f3732aa370ee7c79138634c08ec9
Gary Haran <garyharan@users.noreply.github.com>
2023-11-21 09:07:33 -0500

    Allow usage of Ngrok for remote testing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,8 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
+  config.hosts << "38f59-192-222-198-4.ngrok-free.app"
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
